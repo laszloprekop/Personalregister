52d8829ed2ee032c090a1c185eeb94e0c95bcc02
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-28 14:40:10 +0200

    feat: add Employee model

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+namespace Personalregister;
+
+public class Employee
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public double Salary;
+        public bool Email;
+    }
+}
