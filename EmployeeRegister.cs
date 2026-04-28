9670973fa9eb75c50a9e92d6de158d1e815ffa61
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-28 15:51:08 +0200

    feat: add AddEmployee method to EmployeeRegister

--- EmployeeRegister.cs
+++ EmployeeRegister.cs
@@ -3,4 +3,9 @@ namespace Personalregister;
 public class EmployeeRegister
 {
     public List<Employee> Employees = new List<Employee>();
+    
+    public void AddEmployee(Employee employee)
+    {
+        Employees.Add(employee);
+    }
 }
