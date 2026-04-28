312b3f129842b33ec9a5170264f7d639a8bbec61
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-28 15:07:02 +0200

    refactor: add getters and setters, fix typo in property name

--- Employee.cs
+++ Employee.cs
@@ -2,18 +2,16 @@ namespace Personalregister;
 
 public class Employee
 {
-    public class Employee
-    {
-        public string FirstName;
-        public string LastName;
-        public double Salary;
-        public bool IsDeleted;
-    }
-    public Employee(string firstName, string lastName, double salary, bool IsDeleted = false)
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Salary { get; set; }
+    public bool IsDeleted { get; set; }
+
+    public Employee(string firstName, string lastName, double salary, bool isDeleted = false)
     {
         FirstName = firstName;
         LastName = lastName;
         Salary = salary;
-        issDeleted = IsDeleted;
+        IsDeleted = isDeleted;
     }
 }
