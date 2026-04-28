a423c54fcbc465d8f2f54146e101971975518961
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-28 16:15:55 +0200

    feat: add placeholder ui with static list content

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== Employee Register ===");
+Console.WriteLine();
+Console.WriteLine("1. Sven Svensson - 52,000.00 SEK");
+Console.WriteLine("2. Lars Larsson - 48,000.00 SEK");
+Console.WriteLine();
+Console.WriteLine("[A] Add employee");
+Console.WriteLine("[Q] Quit");
+Console.Write("Enter number to select employee: ");
