929a5525ad2a254829c50edb2685395df293dc65
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-04-28 15:36:51 +0200

    feat: add Input validation for FirstName

--- Employee.cs
+++ Employee.cs
@@ -2,16 +2,28 @@ namespace Personalregister;
 
 public class Employee
 {
-    public string FirstName { get; set; }
+    private string _firstName;
+
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("First name cannot be empty", nameof(value));
+            _firstName = value.Trim();
+        }
+    }
+
     public string LastName { get; set; }
     public double Salary { get; set; }
     public bool IsDeleted { get; set; }
     public string FullName => $"{FirstName} {LastName}";
-    
-    public override string ToString()                                                                
-    {                                                                                                              
-        return $"{FullName} - {Salary:C}";                                                  
-    }  
+
+    public override string ToString()
+    {
+        return $"{FullName} - {Salary:C}";
+    }
 
     public Employee(string firstName, string lastName, double salary, bool isDeleted = false)
     {
