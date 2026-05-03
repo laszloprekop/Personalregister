d73db160cc99ce580ee2287e5b22b1a7271c9097
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-03 14:03:32 +0200

    feat: add single ticket price logic

--- 02-flowcontrol/src/Program.cs
+++ 02-flowcontrol/src/Program.cs
@@ -3,7 +3,7 @@
 while (running)
 {
     Console.Clear();
-    Console.WriteLine("★★★ Flow Control Exercises ★★★");
+    Console.WriteLine("★ ★ ★  Flow Control Exercises  ★ ★ ★ ");
     Console.WriteLine();
     Console.WriteLine("[1] Single ticket price");
     Console.WriteLine("[2] Group ticket price");
@@ -23,7 +23,22 @@ while (running)
             running = false;
             break;
         case 1:
-            Console.WriteLine("(Not yet implemented.)");
+            Console.Clear();
+            Console.WriteLine("★ ★ ★ 2 Single ticket price  ★ ★ ★ ");
+            Console.WriteLine();
+            Console.Write("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+            
+            if (age < 20)
+                Console.WriteLine("Youth Price: 80 kr");
+            else if (age > 64)
+                Console.WriteLine("Senior Price: 90 kr");
+            else
+                Console.WriteLine("Regular Price: 120 kr");
+            
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
             break;
         case 2:
             Console.WriteLine("(Not yet implemented.)");
