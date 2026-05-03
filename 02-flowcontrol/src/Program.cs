44861940b8f42758fd75963ad8b643ee5beb3863
Laszlo Prekop <laszlo.prekop@gmail.com>
2026-05-03 11:31:53 +0200

    prep: add main loop with menu options

--- 02-flowcontrol/src/Program.cs
+++ 02-flowcontrol/src/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool running = true;
+
+while (running)
+{
+    Console.WriteLine("★★★ Flow Control practices ★★★");
+    Console.WriteLine();
+    Console.WriteLine("[1] Single ticket price");
+    Console.WriteLine("[2] Group ticket price");
+    Console.WriteLine("[3] Repeat text");
+    Console.WriteLine("[4] Third word");
+    Console.WriteLine("[0] Quit");
+    Console.WriteLine();
+    Console.Write("Choose an option: ");
+
+
+    int choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 0:
+            running = false;
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+}
